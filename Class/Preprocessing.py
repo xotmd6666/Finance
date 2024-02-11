c9faccba85a23b26482870f1535520b3135e2c74
boring_day <xotmd6666@naver.com>
2024-02-11 10:40:19 +0900

    20240211 필요한 값 읽어오기 모두 완료

--- Class/Preprocessing.py
+++ Class/Preprocessing.py
@@ -1,7 +1,7 @@
 # 클래스 정의
 class Finance_Preprocessing:    
     # 메서드 정의
-    def intermediary_ISA(self, df):
+    def stock(self, df):
         # 첫 번째 행, 열 삭제
         df = df.drop(0)
         df = df.drop('Unnamed: 0', axis = 1)
@@ -18,6 +18,32 @@ class Finance_Preprocessing:
 
         # '날짜' 항목이 '계산 결과'로 되어 있는 행 삭제
         df = df[~(df['날짜'] == '계산 결과')]
+
+        # 인덱스 재설정
+        df = df.reset_index(drop=True)
+
+        return df
+
+    def fund(self, df):
+        # 첫 번째 행을 컬럼명으로 변경 후 첫 번째 행 삭제
+        df.columns = df.iloc[0]
+        df = df.drop(df.index[0])
+
+        # '투자원금' 항목이 비어 있는 행 삭제
+        df = df.drop(df[df['투자원금'].isna() == True].index)
+
+        return df
+
+    def pension(self, df):
+        # 인덱스의 데이터 타입을 문자형으로 변경
+        df.index = df.index.astype(str)
+
+        # 네 번째 인덱스 명을 공백으로 변경
+        df.index.values[3] = ''
+
+        # 네 번째 행을 컬럼명으로 변경 후 필요 없는 행 삭제
+        df.columns = df.iloc[3]
+        df = df.drop(df.index[0:4])
 
         # 인덱스 재설정
         df = df.reset_index(drop=True)
--- Financial State Summary_v1.py
+++ Financial State Summary_v1.py
@@ -8,18 +8,24 @@ from Class.Preprocessing import Finance_Preprocessing
 file_path = r'C:\Users\User\Desktop\Finance\investment_management'
 # 각 자산 관리 파일 읽어오기
 df_intermediary_ISA = pd.read_excel(file_path + r'\intermediary_ISA_account_management.xlsx', sheet_name = '자산 정리')
-df_fund = pd.read_excel(file_path + r'\all_fund_management.xlsx', sheet_name = '전체 총계')
 df_overseas_stock = pd.read_excel(file_path + r'\overseas_stock_management_v8.xlsx', sheet_name = '자산 정리')
+df_fund = pd.read_excel(file_path + r'\all_fund_management.xlsx', sheet_name = '전체 총계')
 df_pension = pd.read_excel(file_path + r'\pension_savings_account_management_v4.xlsm', sheet_name = '예수금 및 자산합계')
 
 # 파일 전처리
 pre = Finance_Preprocessing()
-df_intermediary_ISA = pre.intermediary_ISA(df_intermediary_ISA)
+df_intermediary_ISA = pre.stock(df_intermediary_ISA)
+df_overseas_stock = pre.stock(df_overseas_stock)
+df_fund = pre.fund(df_fund)
+df_pension = pre.pension(df_pension)
 
 # 필요한 데이터 수집
 
-# iloc 인덱서를 사용하여 마지막 행의 특정 컬럼 값 가져오기
+# loc/iloc 인덱서를 사용하여 마지막 행의 특정 컬럼 값 가져오기
 value_intermediary_ISA = df_intermediary_ISA.iloc[-1]['전체 자산 합계(원)']
-print(value_intermediary_ISA)
+value_overseas_stock = int(round(df_overseas_stock.iloc[-1]['전체 자산 합계(원)'], 0))
+value_fund = df_fund.loc[df_fund.index[-1], '평가금액']
+value_pension = df_pension.loc[df_pension.index[-1], '날짜별 전체 자산 합계(원)']
+print(value_pension)
 
 # 재무 정리 파일 기록을 위한 데이터 정리
